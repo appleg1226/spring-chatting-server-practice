f501cd843d20a0b5c4f1b01fcc6e8ccfa28a1bd5
eunchong <appleg1226@gmail.com>
2020-10-03 17:01:02 +0900

    first-import2

---
+++ src/main/java/com/chong/websocketchatting/WebsocketChattingApplication.java
@@ -0,0 +1,13 @@
+package com.chong.websocketchatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChattingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chong/websocketchatting/WebsocketChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chong.websocketchatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
